47f9ccf64d8fd002a2c114378ebd8d2e081c8b43
forsigner <forsigner@gmail.com>
2013-08-31 15:29:49 +0800

    修改js加载方式

--- assets/themes/twitter/static/js/fs.js
+++ assets/themes/twitter/static/js/fs.js
@@ -8835,9 +8835,6 @@ var Forsigner = (function (fs) {
 
     /** 私有属性
      * ----------------------------------------------------*/
-    var _sliding = false,//侧滑的状态
-        _surprise = false,//侧滑的状态
-        _cat = false;//侧滑的状态
 
     /** 私有方法
      * ----------------------------------------------------*/
@@ -8849,56 +8846,49 @@ var Forsigner = (function (fs) {
      */
 
     fs.init = function() {
-        $(function($){
-            var $ctrl = $('#ctrl'),
-                $nav = $('#blog-nav .dib'),
-                $backToTop = $('#back-to-top');
+        var $ctrl = $('#ctrl'),
+            $nav = $('#blog-nav .dib'),
+            $backToTop = $('#back-to-top'),
+            pathname = window.location.pathname;
 
-            $(window).bind("scroll", function(){
-                // 获取网页文档对象滚动条的垂直偏移
-                var scrollTopNum = $(document).scrollTop();
-                // 滚动条的垂直偏移大于 0 时显示，反之隐藏
-                (scrollTopNum > 100) ? $backToTop.fadeIn("fast") : $backToTop.fadeOut("fast");
-            });
+        $(window).bind("scroll", function(){
 
-            // 点击按钮后，滚动条的垂直方向的值逐渐变为0，也就是滑动向上的效果
-            $backToTop.click(function() {
-                $("html, body").animate({ scrollTop: 0 }, 100);
-            });
+            // 获取网页文档对象滚动条的垂直偏移
+            var scrollTopNum = $(document).scrollTop();
 
-            var pathname = window.location.pathname;
-
-            switch (pathname) {
-                case '/':
-                    $nav.removeClass('active');
-                    $nav.eq(0).addClass('active');
-                    break;
-                case '/blog/':
-                    $nav.removeClass('active');
-                    $nav.eq(1).addClass('active');
-                    break;
-                case '/tags/':
-                    $nav.removeClass('active');
-                    $nav.eq(2).addClass('active');
-                    break;
-                case '/about/':
-                    $nav.removeClass('active');
-                    $nav.eq(3).addClass('active');
-                    break;
-                default:
-                    break;
-            }
-
-
-
+            // 滚动条的垂直偏移大于 0 时显示，反之隐藏
+            (scrollTopNum > 100) ? $backToTop.fadeIn("fast") : $backToTop.fadeOut("fast");
         });
 
-        };
-        return fs;
-        } (Forsigner||{}));
+        // 点击按钮后，滚动条的垂直方向的值逐渐变为0，也就是滑动向上的效果
+        $backToTop.click(function() {
+            $("html, body").animate({ scrollTop: 0 }, 100);
+        });
 
+        switch (pathname) {
+            case '/':
+                $nav.removeClass('active');
+                $nav.eq(0).addClass('active');
+                break;
 
+            case '/blog/':
+                $nav.removeClass('active');
+                $nav.eq(1).addClass('active');
+                break;
 
-/*
- * 调用方法
-*/
+            case '/tags/':
+                $nav.removeClass('active');
+                $nav.eq(2).addClass('active');
+                break;
+
+            case '/about/':
+                $nav.removeClass('active');
+                $nav.eq(3).addClass('active');
+                break;
+
+            default:
+                break;
+        }
+    };
+    return fs;
+} (Forsigner||{}));
