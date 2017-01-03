2a0243adfad4951b975f1b17bbfc2ae01dc82d12
子弋 <zhiyi.czy@alibaba-inc.com>
2017-01-03 14:58:54 +0800

    Site updated: 2017-01-03 14:58:54

--- lazying/js/bundle.js
+++ lazying/js/bundle.js
@@ -1,492 +1,117 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj; }
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
-var niceBar = require('./main');
+var lazying = require('./lazying');
 
 (function (factory) {
   if (typeof define === 'function' && define.amd) {
     define([], factory);
   } else if ((typeof window === 'undefined' ? 'undefined' : _typeof(window)) === 'object') {
-    window.niceBar = factory();
+    window.Lazying = factory();
   }
 })(function () {
 
-  return niceBar;
+  return lazying;
 });
 
-},{"./main":9}],2:[function(require,module,exports){
+},{"./lazying":2}],2:[function(require,module,exports){
 'use strict';
 
-var event = require('../util/event');
-var dom = require('../util/dom');
+var winWidth = window.innerWidth || document.documentnode.clientWidth || document.body.clientWidth;
+var winHeight = window.innerHeight || document.documentnode.clientHeight || document.body.clientHeight;
 
-module.exports = function (i) {
+/**
+ *  helper
+ */
 
-  /**
-   * get slider's new top
-   * @param  {number} offsetY  a property from mouseEvent
-   * @return {number} newTop slider's new top
-   */
-  function getNewTop(offsetY) {
-    var newTop;
-
-    if (offsetY < i.sliderY.height / 2) {
-      newTop = 0;
-    } else if (offsetY > i.railY.height - i.sliderY.height / 2) {
-      newTop = i.railY.height - i.sliderY.height;
-    } else {
-      newTop = offsetY - i.sliderY.height / 2;
-    }
-
-    return newTop;
-  }
-
-  /**
-   * update content box scrollTop
-   * @param  {number} newTop      slider's newTop
-   * @param  {number} originTop   slider's originTop
-   * @return null
-   */
-  function updateContent(newTop, originTop) {
-    var journey = newTop - originTop;
-    var scrollTop = journey / i.ratioY;
-    i.content.element.scrollTop += scrollTop;
-    i.sumDeltaY = i.content.element.scrollTop;
-  }
-
-  /**
-   * update slider's css top
-   * @param  {number} newTop slider's new top
-   * @return null
-   */
-  function updateSlider(newTop) {
-    dom.css(i.sliderY.element, 'top', newTop);
-    i.sliderY.deltaY = 0;
-    i.sliderY.top = newTop;
-  }
-
-  /**
-   * clickRailYHandler
-   * @param  {object} e  evnet
-   * @return null
-   */
-  function clickRailYHandler(e) {
-    var originTop = dom.css(i.sliderY.element, 'top');
-    var newTop = getNewTop(e.offsetY);
-
-    updateSlider(newTop);
-    updateContent(newTop, originTop);
-
-    e.preventDefault();
-  }
-
-  event.bind(i.railY.element, 'click', clickRailYHandler);
-};
-
-},{"../util/dom":10,"../util/event":11}],3:[function(require,module,exports){
-'use strict';
-
-var event = require('../util/event');
-var dom = require('../util/dom');
-
-module.exports = function (i) {
-  var originPageY;
-  var originTop;
-  var originScrollTop;
-  var differenceHeight = i.railY.height - i.sliderY.height;
-  var ratioY = i.ratioY;
-
-  event.bind(i.sliderY.element, 'mousedown', function (e) {
-    originPageY = e.pageY;
-    originTop = dom.css(i.sliderY.element, 'top');
-    originScrollTop = i.content.element.scrollTop;
-
-    event.bind(document, 'mousemove', mouseMoveHandler);
-    event.once(document, 'mouseup', mouseUpHandler);
-  });
-
-  /**
-   * mouseMoveHandler
-   * @param  {object} e event
-   * @return null
-   */
-  function mouseMoveHandler(e) {
-    i.showSliderY();
-
-    i.sliderY.deltaY = 0;
-
-    // update slider
-    var newTop = e.pageY - originPageY + originTop;
-
-    if (newTop <= 0) {
-      newTop = 0;
-    } else if (newTop >= differenceHeight) {
-      newTop = differenceHeight;
-    }
-
-    i.sliderY.top = newTop;
-    dom.css(i.sliderY.element, 'top', newTop);
-
-    // udpate content
-    var journey = newTop - originTop;
-    var newScrollTop = journey / ratioY;
-    newScrollTop += originScrollTop;
-    i.content.element.scrollTop = newScrollTop;
-    i.sumDeltaY = i.content.element.scrollTop;
-
-    e.stopPropagation();
-    e.preventDefault();
-  }
-
-  function mouseUpHandler() {
-    event.unbind(document, 'mousemove', mouseMoveHandler);
-
-    i.hideSliderY();
-  }
-};
-
-},{"../util/dom":10,"../util/event":11}],4:[function(require,module,exports){
-'use strict';
-
-var event = require('../util/event');
-
-module.exports = function (i) {
-  event.bind(i.container.element, 'mouseenter', function (e) {
-    i.showSliderY();
-  });
-
-  event.bind(i.container.element, 'mouseleave', function (e) {
-    i.hideSliderY();
-  });
-};
-
-},{"../util/event":11}],5:[function(require,module,exports){
-'use strict';
-
-var event = require('../util/event');
-var dom = require('../util/dom');
-
-module.exports = function (i) {
-
-  function mouseWheelHandler(e) {
-    // update slider
-    i.sumDeltaY += e.deltaY;
-    var newTop = 0;
-
-    if (i.sumDeltaY * i.ratioY < 0) {
-      newTop = 0;
-      i.sliderY.deltaY = 0;
-      i.sumDeltaY = 0;
-    } else if (i.sumDeltaY * i.ratioY > i.railY.height - i.sliderY.height) {
-      newTop = i.railY.height - i.sliderY.height;
-      i.sumDeltaY = i.content.element.scrollHeight - i.content.element.clientHeight;
-    } else {
-      newTop = i.sumDeltaY * i.ratioY;
-    }
-
-    dom.css(i.sliderY.element, 'top', newTop);
-
-    // update box
-    var newScrollTop = 0;
-    newScrollTop += i.sumDeltaY;
-    i.content.element.scrollTop = newScrollTop;
-
-    // i.content.element.scrollTop = i.content.element.scrollTop +  i.sumDeltaY;
-
-    e.preventDefault();
-    e.stopPropagation();
-  }
-
-  event.bind(i.content.element, 'wheel', mouseWheelHandler);
-};
-
-},{"../util/dom":10,"../util/event":11}],6:[function(require,module,exports){
-'use strict';
-
-module.exports = function (i) {
-  // todo
-};
-
-},{}],7:[function(require,module,exports){
-'use strict';
-
-var instance = require('./instance');
-var clickRail = require('./event/click-rail');
-var dragSlider = require('./event/drag-slider');
-var mouseWheel = require('./event/mouse-wheel');
-var pressKeyboard = require('./event/press-keyboard');
-var hoverContainer = require('./event/hover-container');
-
-module.exports = function (element, options) {
-  var i = Object.create(instance);
-  i.init(element, options);
-
-  if (i.content.element.scrollHeight > element.clientHeight) {
-    clickRail(i);
-    dragSlider(i);
-    mouseWheel(i);
-    pressKeyboard(i);
-    hoverContainer(i);
-  }
-};
-
-},{"./event/click-rail":2,"./event/drag-slider":3,"./event/hover-container":4,"./event/mouse-wheel":5,"./event/press-keyboard":6,"./instance":8}],8:[function(require,module,exports){
-'use strict';
-
-var dom = require('./util/dom');
-var guid = require('./util/guid');
-
-var instance = {
-  init: function init(element, options) {
-    if (options) {
-      options = { theme: options.theme || 'light' };
-    } else {
-      options = { theme: 'light' };
-    }
-
-    var $content = createContentElement(element);
-    var $railY = createRailYElement();
-    var $sliderY = createSliderYElement();
-
-    setTheme(element, options);
-
-    dom.appendTo($railY, element);
-    dom.appendTo($sliderY, element);
-
-    this.sumDeltaY = 0;
-
-    this.container = {
-      element: element,
-      width: element.clientWidth,
-      height: element.clientHeight
-    };
-
-    this.ing = true;
-
-    this.content = {
-      deltaY: 0, // 增量
-      element: $content,
-      width: $content.clientWidth,
-      height: $content.scrollHeight,
-      scrollTop: $content.scrollTop
-    };
-
-    this.ratioX = this.container.width / this.content.width;
-    this.ratioY = this.container.height / this.content.height;
-
-    this.railX = { width: 400, height: '' };
-
-    this.railY = {
-      element: $railY,
-      width: 400,
-      height: this.container.height
-    };
-
-    this.sliderX = { width: 400, height: '' };
-
-    this.sliderY = {
-      deltaY: 0, // 增量
-      element: $sliderY,
-      top: 0,
-      width: 40,
-      height: this.container.height * this.ratioY
-    };
-
-    dom.css(this.sliderY.element, 'height', this.sliderY.height + 'px');
-
-    dom.css(this.container.element, {
-      overflow: 'hidden',
-      position: 'relative'
-    });
-
-    dom.css(this.content.element, {
-      overflow: 'hidden',
-      height: this.container.height
-    });
+var util = {
+  hasClass: function hasClass(node, className) {
+    var classes = node.className.split(' ');
+    return classes.indexOf(className) > -1;
   },
 
-  showSliderY: function showSliderY() {
-    dom.addClass(this.sliderY.element, 'fade-in');
-    dom.removeClass(this.sliderY.element, 'fade-out');
-  },
-
-  hideSliderY: function hideSliderY() {
-    dom.addClass(this.sliderY.element, 'fade-out');
-    dom.removeClass(this.sliderY.element, 'fade-in');
-  }
-
-};
-
-// ////////////////////////////////////////
-function setTheme(element, optopns) {
-  dom.addClass(element, 'theme-' + optopns.theme);
-  dom.addClass(element, 'nice-bar');
-}
-
-function createSliderYElement() {
-  return dom.createElement('<div class="nice-bar-slider-y"></div>');
-}
-
-function createRailYElement() {
-  return dom.createElement('<div class="nice-bar-rail-y"></div>');
-}
-
-function createContentElement(element) {
-  var inner = element.innerHTML;
-  var id = guid();
-  element.innerHTML = '<div id="' + id + '"></div>';
-
-  var $content = document.getElementById(id);
-  $content.innerHTML = inner;
-  return $content;
-}
-
-module.exports = instance;
-
-},{"./util/dom":10,"./util/guid":12}],9:[function(require,module,exports){
-'use strict';
-
-var init = require('./init');
-
-module.exports = { init: init };
-
-},{"./init":7}],10:[function(require,module,exports){
-'use strict';
-
-function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj; }
-
-function getCss(element, styleName) {
-  var styleValue = window.getComputedStyle(element)[styleName];
-  if (parseInt(styleValue, 10) || parseInt(styleValue, 10) === 0) {
-    styleValue = parseInt(styleValue, 10);
-  }
-
-  return styleValue;
-}
-
-function setSingleCss(element, styleName, styleValue) {
-  if (typeof styleValue === 'number') {
-    styleValue = styleValue.toString() + 'px';
-  }
-
-  element.style[styleName] = styleValue;
-  return element;
-}
-
-function setMultiCss(element, obj) {
-  for (var key in obj) {
-    var styleValue = obj[key];
-    if (typeof styleValue === 'number') {
-      styleValue = styleValue.toString() + 'px';
-    }
-
-    element.style[key] = styleValue;
-  }
-
-  return element;
-}
-
-var dom = {
-  createElement: function createElement(string) {
-    var element = document.createElement('div');
-    element.innerHTML = string;
-    return element.firstElementChild;
-  },
-
-  appendTo: function appendTo(child, parent) {
-    parent.appendChild(child);
-  },
-
-  addClass: function addClass(element, className) {
-    var classes = element.className.split(' ');
+  addClass: function addClass(node, className) {
+    var classes = node.className.split(' ');
     if (classes.indexOf(className) < 0) {
       classes.push(className);
     }
 
-    element.className = classes.join(' ');
-    return element;
+    node.className = classes.join(' ');
+    return node;
   },
 
-  removeClass: function removeClass(element, className) {
-    var classes = element.className.split(' ');
-    var index = classes.indexOf(className);
-    if (index > -1) {
-      classes.splice(index, 1);
+  forEach: function forEach(arr, fn) {
+    for (var i = 0; i < arr.length; i++) {
+      fn(arr[i], i);
     }
-
-    element.className = classes.join(' ');
-    return element;
   },
 
-  css: function css(element, styleNameOrObject, styleValue) {
-    if ((typeof styleNameOrObject === 'undefined' ? 'undefined' : _typeof(styleNameOrObject)) === 'object') {
-      return setMultiCss(element, styleNameOrObject);
-    } else {
-      if (typeof styleValue === 'undefined') {
-        return getCss(element, styleNameOrObject);
-      } else {
-        return setSingleCss(element, styleNameOrObject, styleValue);
+  filter: function filter(arr, fn) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (fn(arr[i], i)) {
+        result.push(arr[i]);
       }
     }
-  }
 
-};
-
-module.exports = dom;
-
-},{}],11:[function(require,module,exports){
-'use strict';
-
-var event = {
-  bind: function bind(element, name, listener) {
-    element.addEventListener(name, listener, false);
+    return result;
   },
 
-  unbind: function unbind(element, name, listener) {
-    element.removeEventListener(name, listener, false);
-  },
-
-  once: function once(element, name, listener) {
-    var that = this;
-    var once = function once(e) {
-      that.unbind(element, name, once);
-      listener(e);
-    };
-
-    that.bind(element, name, once);
+  bind: function bind(node, name, listener) {
+    node.addEventListener(name, listener, false);
   }
 };
 
-module.exports = event;
-
-},{}],12:[function(require,module,exports){
-'use strict';
-
-module.exports = function () {
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-};
-
-function s4() {
-  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+/**
+ * Public method
+ */
+function getNodes() {
+  var nodes = Array.prototype.slice.call(document.querySelectorAll('[data-src]'));
+  return nodes;
 }
 
+function getOriginSrc(node) {
+  return node.getAttribute('data-src');
+}
+
+function setSrc(node, src) {
+  setTimeout(function () {
+    util.addClass(node, 'loaded');
+    node.setAttribute('src', src);
+  }, 500);
+}
+
+function inView(node) {
+  var box = node.getBoundingClientRect();
+  return box.top < winHeight && box.left > 0 && box.left < winWidth;
+}
+
+/**
+ * Public method
+ */
+function check() {
+  var nodes = util.filter(getNodes(), function (node) {
+    return !util.hasClass(node, 'loaded');
+  });
+
+  util.forEach(nodes, function (node) {
+    if (!inView(node)) {
+      return;
+    }
+
+    setSrc(node, getOriginSrc(node));
+  });
+}
+
+function init() {
+  util.bind(window, 'scroll', check);
+}
+
+module.exports = { init: init, check: check };
+
 },{}]},{},[1]);
-
-(function() {
-  "use strict";
-
-  var str = '<a id="avatar" class="avatar" href="http://forsigner.com/" target="_blank" style="position: absolute;top: 20px;display: inline-block;left: 50%;margin-left:-30px;">' +
-    '<img src="http://forsigner.com/images/avatar.jpg" alt="forsigner" title="go home" style="width: 60px; height: 60px;border-radius: 50%;">' +
-    '</a>';
-
-  var $div = document.createElement('div');
-  $div.innerHTML = str;
-  document.body.appendChild($div);
-
-
-}());
 
 'use strict';
 
@@ -1037,7 +662,8 @@ var data = [{
 }];
 
 // light
-(function() {
+(function () {
+  data = data.concat(data).concat(data).concat(data);
   var $container = document.getElementById('container-light');
 
   var content = '';
@@ -1046,7 +672,7 @@ var data = [{
   for (var i = 0, length = data.length; i < length; i++) {
     var li = '<li>' +
       '<a target="_blank" href="' + data[i].html_url + '">' +
-      '<img class="avatar" src="' + data[i].avatar_url + '" />' +
+      '<img class="avatar" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC" data-src="' + data[i].avatar_url + '" />' +
       '<span class="nickname">' + data[i].login + '</span>' +
       '</a>' +
       '<li>';
@@ -1055,28 +681,6 @@ var data = [{
 
   ul.innerHTML = content;
 
-  niceBar.init($container);
-}());
-
-
-// dark
-(function() {
-  var $container = document.getElementById('container-dark');
-
-  var content = '';
-  var ul = document.getElementById('list-dark');
-
-  for (var i = 0, length = data.length; i < length; i++) {
-    var li = '<li>' +
-      '<a target="_blank" href="' + data[i].html_url + '">' +
-      '<img class="avatar" src="' + data[i].avatar_url + '" />' +
-      '<span class="nickname">' + data[i].login + '</span>' +
-      '</a>' +
-      '<li>';
-    content += li;
-  }
-
-  ul.innerHTML = content;
-
-  niceBar.init($container, {theme: 'dark'});
+  Lazying.init();
+  Lazying.check();
 }());
